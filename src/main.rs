c29dc33cf9e80a35e63bb3b1e9ba1a3b8812a6ae
Matt Spencer <matthew@thespencers.me.uk>
2022-02-12 20:33:00 -0700

    Initial checkin of Rust based snake

---
+++ src/main.rs
@@ -0,0 +1,301 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use std::cmp::min;
+use log::error;
+use instant::Instant;
+use std::time::Duration;
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent, StartCause, VirtualKeyCode, ElementState},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use pixels::{Pixels, SurfaceTexture, wgpu::Color};
+use line_drawing::Bresenham;
+use rand::seq::SliceRandom;
+
+const WIDTH: usize = 32;
+const HEIGHT: usize = 24;
+
+#[derive(Copy, Clone, Debug)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+#[derive(Copy, Clone, Debug, Default, PartialEq)]
+struct Point {
+    x : i32,
+    y : i32,
+}
+
+impl Point {
+    const fn new(x: i32, y: i32) ->  Self {
+        Self { x, y }
+    }
+
+    fn move_in_direction(&mut self, direction: Direction) {
+        match direction {
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1,
+            Direction::Up => self.y -= 1,
+            Direction::Down => self.y += 1,
+        }
+    }
+}
+
+struct Snake {
+    points: Vec<Point>,
+}
+
+impl Snake {
+    fn new() -> Self {
+        Self {
+            points: vec![Point::new(10,10), Point::new(9, 10), Point::new(8,10), Point::new(7,10)],
+        }
+    }
+
+    fn contains(&self, point: Point) -> bool {
+        self.points.iter().any( |&p| p == point)
+    }
+
+    fn get_head(&self) -> Point {
+        self.points[0]
+    }
+}
+
+struct World {
+    width: usize,
+    height: usize,
+
+    last_direction: Direction,
+    tick_speed: Duration,
+
+    snake: Snake,
+}
+
+impl World {
+    fn new() -> Self {
+        Self {
+            width: WIDTH,
+            height: HEIGHT,
+
+            last_direction: Direction::Right,
+            tick_speed: Duration::new(1, 0),
+
+            snake: Snake::new()
+        }
+    }
+
+    fn tick(&mut self) {
+        if self.can_move_in_direction(self.last_direction) {
+            self.move_in_direction(self.last_direction);
+        } else {
+            println!("GAME OVER");
+        }
+    }
+
+    fn hit_wall(&self, point: Point) -> bool {
+        point.x == 0 || point.x == (self.width - 1) as i32 ||
+        point.y == 0 || point.y == (self.height - 1) as i32
+    }
+
+    fn hit_food(&self, _point: Point) -> bool {
+        false
+    }
+
+    fn can_move_in_direction(&self, direction: Direction) -> bool {
+        let mut new_head = self.snake.get_head();
+        new_head.move_in_direction(direction);
+
+        let hit_self = self.snake.contains(new_head);
+        let hit_wall = self.hit_wall(new_head);
+
+        !(hit_self || hit_wall)
+    }
+    
+    fn move_in_direction(&mut self, direction: Direction) {
+        // Create a new point that will become the new head
+        let mut head = self.snake.get_head();
+        head.move_in_direction(direction);
+
+        // Place it at the head of the list
+        self.snake.points.insert(0,head);
+
+        // Pop the last point of the snake list
+        if !self.hit_food(head) {
+            self.snake.points.pop();
+        }
+    }
+
+
+    fn is_free(&self, point: Point) -> bool {
+        let hit_snake = self.snake.contains(point);
+        let hit_wall = self.hit_wall(point);
+        let hit_food = self.hit_food(point);
+
+        !hit_snake && !hit_wall && !hit_food
+    }
+
+    //
+    fn get_free_spaces(&self) -> Vec<Point> {
+        let mut free=Vec::new();
+
+        for x in 0..self.width-1 {
+            for y in 0..self.height-1 {
+                let point = Point::new(x as i32, y as i32);
+                if self.is_free(point) {
+                    free.push(point);
+                }
+            }
+        }
+
+        free
+    }
+
+    fn add_food(&self) {
+        let free_spaces=self.get_free_spaces();
+
+        let mut rng = rand::thread_rng();
+        let point = free_spaces.choose(&mut rng);
+
+        println!{"New food at {:?}", point};
+    }
+
+
+
+    // TODO:: Move these into a rendering class
+    fn point(&self, frame: &mut [u8], point: &Point, colour: [u8; 4]) {
+        let x = min(point.x as usize, self.width - 1);
+        let y = min(point.y as usize, self.height - 1);
+        let i = x * 4 + y * WIDTH * 4;
+
+        frame[i..i + 4].copy_from_slice(&colour);
+    }
+
+    fn line(&self, frame: &mut [u8], start: Point, end: Point, colour: [u8; 4]) {
+        let p1 = (start.x as i32, start.y as i32);
+        let p2 = (end.x as i32, end.y as i32);
+    
+        for (x, y) in Bresenham::new(p1, p2) {
+            self.point(frame, &Point::new(x, y), colour);
+        }
+    }
+
+    fn rect(&self, frame: &mut[u8], p0: Point, p3: Point, colour: [u8; 4]) {
+        let p1 = Point::new(p3.x, p0.y);
+        let p2 = Point::new(p0.x, p3.y);
+
+        self.line(frame, p0, p1, colour);
+        self.line(frame, p1, p3, colour);
+        self.line(frame, p3, p2, colour);
+        self.line(frame, p2, p0, colour);
+    }
+
+    fn draw(&self, frame: &mut [u8] ) {
+        let black = [0x00, 0x00, 0x00, 0xff];
+        let red = [0xff, 0x00, 0x00, 0xff];
+        let white = [0xff, 0xff, 0xff, 0xff];
+
+        for pixel in frame.chunks_exact_mut(4) {
+            pixel.copy_from_slice(&black);
+        }
+
+        let x0 = Point::new(0,0);
+        let x1= Point::new((self.width-1) as i32,(self.height-1) as i32);
+
+        self.rect(frame, x0, x1, red);
+
+        self.line(frame, Point::new(0,0), Point::new((self.width-1) as i32,0), red);
+        
+        for point in &self.snake.points {
+            self.point(frame, point, white);
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new();
+
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let inner_size = LogicalSize::new((WIDTH*20) as f64, (HEIGHT*20) as f64);
+        WindowBuilder::new()
+            .with_title("Rusty Snake")
+            .with_inner_size(inner_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let size = window.inner_size();
+        let surface = SurfaceTexture::new(size.width, size.height, &window);
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface).unwrap()
+    };
+
+    pixels.set_clear_color(Color::BLACK);
+
+    let mut world = World::new();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::RedrawRequested(_) =>  {
+                world.draw(pixels.get_frame());
+
+                if pixels
+                    .render()
+                    .map_err(|e| error!("pixels.render() failed: {}", e))
+                    .is_err() {
+                    *control_flow = ControlFlow::Exit;
+                }
+            },
+
+            Event::NewEvents(StartCause::Init) => {
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + world.tick_speed )
+            },
+            Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + world.tick_speed );
+                world.tick();
+                window.request_redraw();
+            },
+
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    device_id: _, input: kin, is_synthetic: _
+                } => {
+                    if kin.state == ElementState::Pressed {
+                        match kin.virtual_keycode {
+                            Some(VirtualKeyCode::A) => world.last_direction=Direction::Left,
+                            Some(VirtualKeyCode::S) => world.last_direction=Direction::Down,
+                            Some(VirtualKeyCode::W) => world.last_direction=Direction::Up,
+                            Some(VirtualKeyCode::D) => world.last_direction=Direction::Right,
+
+                            Some(VirtualKeyCode::P) => world.add_food(),
+                            _ => ()
+                        }
+                    }
+                },
+                WindowEvent::Resized(size) =>  {
+                    pixels.resize_surface(size.width, size.height);
+                    window.request_redraw();
+                    
+                },
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => ()
+            },
+
+            // Event::DeviceEvent { event, .. } => match event {
+            //     _ => {
+            //         //println!("Device event {:?}", event);
+            //     }
+            // }
+        
+            _ => (),
+        }
+    });
+}
+
